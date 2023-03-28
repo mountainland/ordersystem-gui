611633b949ac59ed4f69aa26dd8ae67aeb42ffba
Oliver <vuoreol@pm.me>
2023-03-28 16:11:07 +0300

    Update main.py
    Signed-off-by: Oliver <vuoreol@pm.me>

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
 import tkinter
+import tkinter as tk
+from tkinter import ttk
 import tkinter.messagebox
 import customtkinter
+import requests
+import traceback
+global LOGGED_IN
+LOGGED_IN = False
 
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
-
+class LoginWindow(customtkinter.CTkToplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.title("Login")
+        self.geometry("300x500")
+        self.attributes('-fullscreen', True)
+        self.protocol("WM_DELETE_WINDOW", lambda: None)
+        self.bind("<Alt-F4>", lambda event: None)
+        self.bind("<Control-w>", lambda event: None)
+        self.bind("<Control-W>", lambda event: None)
+        
+        # Create login form
+        self.username_label = customtkinter.CTkLabel(self, text="Username:")
+        self.username_label.pack(pady=10)
+        
+        self.username_entry = customtkinter.CTkEntry(self)
+        self.username_entry.pack(pady=5)
+        
+        self.password_label = customtkinter.CTkLabel(self, text="Password:")
+        self.password_label.pack(pady=10)
+        
+        self.password_entry = customtkinter.CTkEntry(self, show="*")
+        self.password_entry.pack(pady=5)
+        
+        self.login_button = customtkinter.CTkButton(self, text="Login", command=self.login)
+        self.login_button.pack(pady=10)
+        
+        self.error_label = customtkinter.CTkLabel(self)
+    
+    def login(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+        
+        # TODO: Validate username and password with backend API
+        
+        if username == "admin" and password == "password":
+            self.destroy()
+            self.logged_in = True
+            global LOGGED_IN
+            LOGGED_IN = True
+        else:
+            self.error_label.config(text="Invalid username or password")
+            self.error_label.pack(pady=10)
 
 class OrderApp(customtkinter.CTkToplevel):
     def __init__(self, *args, **kwargs):
@@ -14,20 +60,76 @@ class OrderApp(customtkinter.CTkToplevel):
         self.label = customtkinter.CTkLabel(self, text="Order")
         self.label.pack(padx=20, pady=20)
 
-        self.build_menu()
-
-    def start_ordering(self):
-        print("mau")    
+        self.build_main()
     
-    #def send_order(self):
+    def build_main(self):
+        products_url = f"https://api.ordersystem.luova.club/products/"
+        product_response = requests.get(products_url)
+        product_response.raise_for_status()
+        products_data = product_response.json()
+        products = products_data["products"]
+        
+        for product in products:
+            label = customtkinter.CTkLabel(master=self, text=f'{product["Name"]} {product["Price"]}€')
+            label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+            entry = customtkinter.CTkEntry(master=self, placeholder_text=f"1")
+            entry.pack(padx=20, pady=10)
+            
+            
+    def fetch_customer_info(self):
+        # Get customer ID from entry widget
+        customer_id = self.customer_id_entry.get()
+        
+        try:
+            # Make API request to get customer info
+            customer_url = f"https://api.ordersystem.luova.club/customer/{customer_id}"
+            customer_response = requests.get(customer_url)
+            customer_response.raise_for_status()
+            customer_data = customer_response.json()
 
-
-    def build_menu(self):
-        button = customtkinter.CTkButton(master=self, text="CTkButton", command=self.start_ordering)
-        button.pack(padx=20, pady=10)
-        entry = customtkinter.CTkEntry(master=self, placeholder_text="CTkEntry")
-        entry.pack(padx=20, pady=10)
-
+            # Create popup window with customer info and editing fields
+            popup_window = customtkinter.CTkToplevel()
+            popup_window.title("Customer Info")
+            popup_window.geometry("300x500")
+            
+            first_name_label = customtkinter.CTkLabel(popup_window, text="First Name:")
+            first_name_label.pack(pady=5)
+            first_name_entry = customtkinter.CTkEntry(popup_window)
+            first_name_entry.pack(pady=5)
+            first_name_entry.insert(0, customer_data.get('FirstName'))
+            
+            last_name_label = customtkinter.CTkLabel(popup_window, text="Last Name:")
+            last_name_label.pack(pady=5)
+            last_name_entry = customtkinter.CTkEntry(popup_window)
+            last_name_entry.pack(pady=5)
+            last_name_entry.insert(0, customer_data.get('LastName'))
+            
+            balance_label = customtkinter.CTkLabel(popup_window, text="Balance:")
+            balance_label.pack(pady=5)
+            balance_entry = customtkinter.CTkEntry(popup_window)
+            balance_entry.pack(pady=5)
+            balance_entry.insert(0, customer_data.get('Balance'))
+            
+            save_button = customtkinter.CTkButton(popup_window, text="Save", command=lambda: self.save_customer_info(customer_id, first_name_entry.get(), last_name_entry.get(), balance_entry.get()))
+            save_button.pack(pady=10)
+            
+            close_button = customtkinter.CTkButton(popup_window, text="Close", command=popup_window.destroy)
+            close_button.pack(pady=10)
+        except requests.exceptions.RequestException as e:
+            # Show error message and technical details in popup window
+            popup_window = tk.Toplevel()
+            popup_window.title("Error")
+            popup_window.geometry("300x200")
+            
+            error_label = customtkinter.CTkLabel(popup_window, text=f"Error: {e}")
+            error_label.pack(pady=10)
+            
+            show_details_button = customtkinter.CTkButton(popup_window, text="Show Technical Details", command=lambda: self.show_technical_details(traceback.format_exc()))
+            show_details_button.pack(pady=10)
+            
+            close_button = customtkinter.CTkButton(popup_window, text="Close", command=popup_window.destroy)
+            close_button.pack(pady=10)
+    
 class CustomerApp(customtkinter.CTkToplevel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -36,6 +138,133 @@ class CustomerApp(customtkinter.CTkToplevel):
         self.label = customtkinter.CTkLabel(self, text="Customer")
         self.label.pack(padx=20, pady=20)
 
+        self.build_menu()
+
+    def build_menu(self):
+        # Create customer ID label and entry
+        self.customer_id_label = customtkinter.CTkLabel(self, text="Customer ID:")
+        self.customer_id_label.pack(pady=10)
+        
+        self.customer_id_entry = customtkinter.CTkEntry(self)
+        self.customer_id_entry.pack(pady=5)
+        
+        # Create button to fetch customer info
+        self.fetch_button = customtkinter.CTkButton(self, text="Fetch Customer Info", command=self.fetch_customer_info)
+        self.fetch_button.pack(pady=10)
+
+        # Create button to save customer info
+        self.save_button = customtkinter.CTkButton(self, text="Save Customer Info", command=self.save_customer_info)
+        self.save_button.pack(pady=10)
+        
+        # Create exit button
+        self.exit_button = customtkinter.CTkButton(self, text="Exit", command=self.destroy)
+        self.exit_button.pack(pady=10)
+        
+    def fetch_customer_info(self):
+        # Get customer ID from entry widget
+        customer_id = self.customer_id_entry.get()
+        
+        try:
+            # Make API request to get customer info
+            customer_url = f"https://api.ordersystem.luova.club/customer/{customer_id}"
+            customer_response = requests.get(customer_url)
+            customer_response.raise_for_status()
+            customer_data = customer_response.json()
+
+            # Create popup window with customer info and editing fields
+            popup_window = customtkinter.CTkToplevel()
+            popup_window.title("Customer Info")
+            popup_window.geometry("300x500")
+            
+            first_name_label = customtkinter.CTkLabel(popup_window, text="First Name:")
+            first_name_label.pack(pady=5)
+            first_name_entry = customtkinter.CTkEntry(popup_window)
+            first_name_entry.pack(pady=5)
+            first_name_entry.insert(0, customer_data.get('FirstName'))
+            
+            last_name_label = customtkinter.CTkLabel(popup_window, text="Last Name:")
+            last_name_label.pack(pady=5)
+            last_name_entry = customtkinter.CTkEntry(popup_window)
+            last_name_entry.pack(pady=5)
+            last_name_entry.insert(0, customer_data.get('LastName'))
+            
+            balance_label = customtkinter.CTkLabel(popup_window, text="Balance:")
+            balance_label.pack(pady=5)
+            balance_entry = customtkinter.CTkEntry(popup_window)
+            balance_entry.pack(pady=5)
+            balance_entry.insert(0, customer_data.get('Balance'))
+            
+            save_button = customtkinter.CTkButton(popup_window, text="Save", command=lambda: self.save_customer_info(customer_id, first_name_entry.get(), last_name_entry.get(), balance_entry.get()))
+            save_button.pack(pady=10)
+            
+            close_button = customtkinter.CTkButton(popup_window, text="Close", command=popup_window.destroy)
+            close_button.pack(pady=10)
+        except requests.exceptions.RequestException as e:
+            # Show error message and technical details in popup window
+            popup_window = tk.Toplevel()
+            popup_window.title("Error")
+            popup_window.geometry("300x200")
+            
+            error_label = customtkinter.CTkLabel(popup_window, text=f"Error: {e}")
+            error_label.pack(pady=10)
+            
+            show_details_button = customtkinter.CTkButton(popup_window, text="Show Technical Details", command=lambda: self.show_technical_details(traceback.format_exc()))
+            show_details_button.pack(pady=10)
+            
+            close_button = customtkinter.CTkButton(popup_window, text="Close", command=popup_window.destroy)
+            close_button.pack(pady=10)
+    
+    def save_customer_info(self, customer_id, first_name, last_name, balance):
+        # Create payload data
+        payload = {
+            "FirstName": first_name,
+            "LastName": last_name,
+            "Balance": balance
+        }
+        
+        try:
+            # Make API request to update customer info
+            customer_url = f"https://api.ordersystem.luova.club/customer/{customer_id}"
+            customer_response = requests.post(customer_url, json=payload)
+            customer_response.raise_for_status()
+
+            # Show success message in popup window
+            popup_window = tk.Toplevel()
+            popup_window.title("Success")
+            popup_window.geometry("300x200")
+            
+            success_label = customtkinter.CTkLabel(popup_window, text="Customer info saved successfully.")
+            success_label.pack(pady=10)
+            
+            close_button = customtkinter.CTkButton(popup_window, text="Close", command=popup_window.destroy)
+            close_button.pack(pady=10)
+        except requests.exceptions.RequestException as e:
+            # Show error message and technical details in popup window
+            popup_window = tk.Toplevel()
+            popup_window.title("Error")
+            popup_window.geometry("300x200")
+            
+            error_label = customtkinter.CTkLabel(popup_window, text=f"Error: {e}")
+            error_label.pack(pady=10)
+            
+            show_details_button = customtkinter.CTkButton(popup_window, text="Show Technical Details", command=lambda: self.show_technical_details(traceback.format_exc()))
+            show_details_button.pack(pady=10)
+            
+            close_button = customtkinter.CTkButton(popup_window, text="Close", command=popup_window.destroy)
+            close_button.pack(pady=10)
+        
+    def show_technical_details(self, technical_details):
+        # Show technical details in popup window
+        popup_window = tk.Toplevel()
+        popup_window.title("Technical Details")
+        popup_window.geometry("500x300")
+        
+        technical_details_label = customtkinter.CTkLabel(popup_window, text=technical_details)
+        technical_details_label.pack(pady=10)
+        
+        close_button = customtkinter.CTkButton(popup_window, text="Close", command=popup_window.destroy)
+        close_button.pack(pady=10)
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -43,13 +272,47 @@ class App(customtkinter.CTk):
         # configure window
         self.title("CustomTkinter complex_example.py")
         self.geometry(f"{1100}x{580}")
-
-        self.build_main()
-
+        
         self.order_window = None
 
         self.customer_window = None
+        
+        #self.withdraw()
+        self.logged_in = True
 
+        
+        
+        self.show_login_window()
+
+    def show_login_window(self):
+        #self.withdraw()  # hide the main window
+        self.login_window = LoginWindow(self)
+        self.wait_window(self.login_window)
+
+        if self.logged_in or LOGGED_IN:
+            self.deiconify()  # show the main window
+            self.build_main()
+        else:
+            self.show_login_window()
+        
+        
+
+    def logout(self):
+        self.logged_in = False
+        global LOGGED_IN
+        LOGGED_IN = False
+
+        # Destroy all widgets in the main window
+        for widget in self.winfo_children():
+            widget.destroy()
+
+        # Reset customer ID entry
+        #self.customer_id_entry.delete(0, tk.END)
+
+        # Hide the main window and show the login window
+        self.withdraw()
+        self.show_login_window()
+    
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
         print("CTkInputDialog:", dialog.get_input())
@@ -95,7 +358,7 @@ class App(customtkinter.CTk):
         self.sidebar_button_order.grid(row=1, column=0, padx=20, pady=10)
         self.sidebar_button_customer = customtkinter.CTkButton(self.sidebar_frame, command=self.open_customer_menu, text="Customer")
         self.sidebar_button_customer.grid(row=2, column=0, padx=20, pady=10)
-        self.sidebar_button_logout = customtkinter.CTkButton(self.sidebar_frame, command=quit, text="Logout")
+        self.sidebar_button_logout = customtkinter.CTkButton(self.sidebar_frame, command=self.logout, text="Logout")
         self.sidebar_button_logout.grid(row=3, column=0, padx=20, pady=10)
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
